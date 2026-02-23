69677e73632ceb8f81b577ae4ee0ac0c021de7e0
Guilherme da Silva Sousa <guiihsilvasousa1@gmail.com>
2026-02-23 18:32:25 -0300

    Delete Digital-Tech-Solutions-main directory

--- Digital-Tech-Solutions-main/JS/login.js
+++
@@ -1,8 +0,0 @@
- document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("loginForm");
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Login enviado! (simulação)");
-  });
-});
